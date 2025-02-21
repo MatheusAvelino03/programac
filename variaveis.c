b820889a22843a5cd8e1994bb7dfc1fa25450f2b
Matheus Avelino <127220952+MatheusAvelino03@users.noreply.github.com>
2025-02-21 01:34:30 +0000

    arquivo 2

---
+++ variaveis.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // Sempre inicialiar a variavél dando valores a ela;
+
+    // não pode começar a variavél com número;
+    // Não pode usar palavras reservadas - pode-se dar conflitos com a compilação;
+    //  "=" é um sinal de atribuição de valores para a varivél;
+    int idade = 24;
+    int quantidade = 34;
+    //"float"- valores menores
+    float altura = 1.75;
+    //"char"- para pode escrever um conjuntos de carateres, tem que definir um array"[]";
+    //"Array [] - sempre definir o tamanho dos dados que iram ser precisos usar"
+    char letra = 'A';
+    char nome[20] = "Matheus Avelino";
+    //"double" - armazena valores maiores;
+    double comprimento;
+
+    idade = 25;
+}
